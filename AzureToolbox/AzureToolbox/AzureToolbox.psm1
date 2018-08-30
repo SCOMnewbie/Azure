7ba963448912c5f9fc3d0edfa7b8def7b5e47e33
SCOMnewbie <leon.francois75@gmail.com>
2018-08-30 22:56:51 +0200

    A

---
+++ AzureToolbox/AzureToolbox/AzureToolbox.psm1
@@ -0,0 +1,24 @@
+#Get public and private function definition files.
+$Public = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
+$Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
+
+#Dot source the files
+Foreach ($import in @($Public + $Private)) {
+    Try {
+        . $import.fullname
+    }
+    Catch {
+        Write-Error -Message "Failed to import function $($import.fullname): $_"
+    }
+}
+
+If ((Compare-Module -Name AzureRM).UpdateNeeded) {
+    Write-Warning -Message "The AzureRM Module has an update available on PSGallery"
+}
+
+# Here I might...
+# Read in or create an initial config file and variable
+# Export Public functions ($Public.BaseName) for WIP modules
+# Set variables visible to the module and its functions only
+
+Export-ModuleMember -Function $Public.Basename
