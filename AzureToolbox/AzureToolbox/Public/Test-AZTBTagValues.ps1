ee2a9300595472db06fb5c90904dfd5e3c170a5a
SCOMnewbie <leon.francois75@gmail.com>
2018-09-05 23:15:49 +0200

    Stable version

---
+++ AzureToolbox/AzureToolbox/Public/Test-AZTBTagValues.ps1
@@ -0,0 +1,106 @@
+Function Test-AZTBTagValues { 
+    <#
+.SYNOPSIS
+	Function to validate Tag on a specific resource
+.DESCRIPTION
+	Function to validate Tag on a specific resource
+.PARAMETER BillTo
+    Specifies the name of the BillTo. This is a mandatory field based on a list of choices. All Management codes as to be pre-filled.
+.PARAMETER Department
+	Specifies the department. This is a mandatory field. Word in uppercase only between 4 and 11 characters without whitespace.
+.PARAMETER Tier
+    Specifies the tier. This is an mandatory field based on a list of choices.
+.PARAMETER Environement
+    Specifies the Environement. This is a mandatory field where you must have only a list of choices of your Environement.
+.PARAMETER Owner
+Specifies the Owner of the resource group. This is a mandatory field where you must specify a UPN.
+.PARAMETER ApplicationName
+    Specifies the applicationname. This is an optionnal field. The length should be between 4 and 20 characters with the first letter in Uppercase
+    and the rest in lowercase without whitespace.
+.EXAMPLE
+	$params = @{
+    'BillTo'='BillCode1'
+	'Department'='TEST'
+    'ApplicationName'='Myapps'
+    'Environement'='PROD'
+    'Tier'='Application Tier'
+    'Owner'='francois.leon@company.com'
+    }
+    Test-AZTBTagValues @params
+.EXAMPLE
+    Test-AZTBTagValues -BillTo BillCode1 -Department TEST -Environement DEV -Tier 'Application Tier' -Owner francois.leon@company.com                             
+.NOTES
+	Francois LEON
+	https://scomnewbie.wordpress.com/
+	github.com/ScomNewbie
+.LINK
+	https://docs.microsoft.com/fr-fr/azure/architecture/cloud-adoption/appendix/azure-scaffold?toc=%2Fen-us%2Fazure%2Fazure-resource-manager%2Ftoc.json&bc=%2Fen-us%2Fazure%2Fbread%2Ftoc.json#resource-tags
+
+    #>
+
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName)] 
+        [ValidateNotNullOrEmpty()]
+        [ValidateSet("BillCode1", "BillCode2", IgnoreCase = $false)] 
+        [String] 
+        $BillTo 
+        , 
+        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName)] 
+        [ValidateNotNullOrEmpty()]
+        [ValidateScript( {
+                if ( $_ -cmatch '^[A-Z][A-Z0-9]{2,10}') {
+                    $true
+                }
+                else {
+                    throw 'Please provide a valid Department all in UpperCase and between 3 and 11 characters (ex TEAMX,FINANCE,RB6 )'
+                }
+            })] 
+        [String]
+        $Department  
+        , 
+        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName)] 
+        [ValidateNotNullOrEmpty()]
+        [ValidateSet("PROD", "DEV", "UAT", IgnoreCase = $false)] 
+        [string] 
+        $Environement
+        , 
+        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName)] 
+        [ValidateNotNullOrEmpty()]
+        [ValidateSet("Web Tier", "Application Tier", "Database", IgnoreCase = $false)] 
+        [string] 
+        $Tier
+        , 
+        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName)] 
+        [ValidateNotNullOrEmpty()]
+        [ValidateScript( {
+                #Not a perfect regex but will be OK because we will take the current UPN
+                if ( $_ -match '^.+@.+\..+$') {
+                    $true
+                }
+                else {
+                    throw 'Please provide a valid address mail (UPN)'
+                }
+            })] 
+        [String]
+        $Owner
+        , 
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName)] 
+        [ValidateScript( {
+                if ( $_ -cmatch '^[A-Z][a-z0-9]{3,19}') {
+                    $true
+                }
+                else {
+                    throw 'Please provide a valid application name. First letter UpperCase and the rest LowerCase. Between 4 and 20 characters (ex Myapp, Superapp )'
+                }
+            })] 
+        [string] 
+        $ApplicationName
+    ) 
+    Process { 
+        Write-Verbose "Start validate Tags"
+        #Add -Tag as the end + output = object with 2 properties Isvalidated + OutputName
+        $true
+        Write-Verbose "End validate Tags"
+    } 
+}
---
+++ AzureToolbox/AzureToolbox/Tests/Test-AZTBTagValues.Tests.ps1
@@ -0,0 +1,121 @@
+<#
+.Synopsis
+    Pester test to verify the function Test-AZTBTagValues.
+.Description
+    Pester test to verify the function Test-AZTBTagValues.
+
+#>
+[CmdletBinding()]
+PARAM(
+    $ModuleName = "AzureToolbox"
+)
+
+
+# Make sure one or multiple versions of the module are note loaded
+Get-Module -Name $ModuleName | remove-module
+
+# Find the Manifest file
+$ManifestFile = "$(Split-Path -Parent -Path (Split-Path -Parent -Path $MyInvocation.MyCommand.Definition))\$ModuleName.psd1"
+$ModuleFile = "$(Split-Path -Parent -Path (Split-Path -Parent -Path $MyInvocation.MyCommand.Definition))\$ModuleName.psm1"
+
+# Import the module and store the information about the module
+Import-module -Name $ManifestFile -PassThru
+Import-module -Name $ModuleFile -PassThru
+
+Describe "Test-AZTBTagValues Function" {
+
+    Context 'Function Output' {
+        $params = @{
+            'BillTo'          = 'billCode1'
+            'Department'      = 'TEST'
+            'ApplicationName' = 'Myapps'
+            'Environement'    = 'PROD'
+            'Tier'            = 'Application Tier'
+            'Owner'           = 'francois.leon@company.com'
+        }
+
+        It 'Bad BillTo spelling should throw' {
+            {Test-AZTBTagValues @params }|Should throw
+        }
+
+        $params = @{
+            'BillTo'          = 'BillCode1'
+            'Department'      = 'tEST'
+            'ApplicationName' = 'Myapps'
+            'Environement'    = 'PROD'
+            'Tier'            = 'Application Tier'
+            'Owner'           = 'francois.leon@company.com'
+        }
+
+        It 'Bad department spelling should throw' {
+            {Test-AZTBTagValues @params }|Should throw
+        }
+
+        $params = @{
+            'BillTo'          = 'BillCode1'
+            'Department'      = 'TEST'
+            'ApplicationName' = 'myapps'
+            'Environement'    = 'PROD'
+            'Tier'            = 'Application Tier'
+            'Owner'           = 'francois.leon@company.com'
+        }
+
+        It 'Bad applicationName spelling should throw' {
+            {Test-AZTBTagValues @params }|Should throw
+        }
+
+        $params = @{
+            'BillTo'          = 'BillCode1'
+            'Department'      = 'TEST'
+            'ApplicationName' = 'Myapps'
+            'Environement'    = 'ROD'
+            'Tier'            = 'Application Tier'
+            'Owner'           = 'francois.leon@company.com'
+        }
+
+        It 'Bad Environement spelling should throw' {
+            {Test-AZTBTagValues @params }|Should throw
+        }
+
+        $params = @{
+            'BillTo'          = 'BillCode1'
+            'Department'      = 'TEST'
+            'ApplicationName' = 'Myapps'
+            'Environement'    = 'PROD'
+            'Tier'            = 'Application'
+            'Owner'           = 'francois.leon@company.com'
+        }
+
+        It 'Bad Tier spelling should throw' {
+            {Test-AZTBTagValues @params }|Should throw
+        }
+
+        $params = @{
+            'BillTo'          = 'BillCode1'
+            'Department'      = 'TEST'
+            'ApplicationName' = 'Myapps'
+            'Environement'    = 'PROD'
+            'Tier'            = 'Application Tier'
+            'Owner'           = 'francois.leon@company'
+        }
+
+        It 'Bad Owner spelling should throw' {
+            {Test-AZTBTagValues @params }|Should throw
+        }
+
+        $params = @{
+            'BillTo'          = 'BillCode1'
+            'Department'      = 'TEST'
+            'ApplicationName' = 'Myapps'
+            'Environement'    = 'PROD'
+            'Tier'            = 'Application Tier'
+            'Owner'           = 'francois.leon@company.com'
+        }
+
+        It 'GOOD spelling should Be OK' {
+            $Test = Test-AZTBTagValues @params
+            $Test|Should be true
+        }
+    }
+}
+
