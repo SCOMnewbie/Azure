1251d41261fd74caef9a783e4aee78f28b2174c6
Francois LEON <francois.leon@ubisoft.com>
2020-10-14 13:18:09 +0200

    Add reference to my Azure devops shift left project

---
+++ Start-ShiftLeftProject.ps1
@@ -0,0 +1,5 @@
+<#
+    Function which will redirect to a funny shift left ARM deployment public project where I play with several tools like Super-Linter, Gitleaks, Pester, AzCli to just name a few.
+#>
+
+Start-Process -FilePath "https://clt-100ba0e8-bfd0-402c-bcb5-c6dc4d62ba6c.visualstudio.com/_git/LearnShiftLeft-pub"
